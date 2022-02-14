c78c67fc19fc0c2a5aae725fe6bef698542185a9
bossman-codex <56489079+bossman-codex@users.noreply.github.com>
2022-02-14 21:29:36 +0100

    added postgres

--- script.js
+++ script.js
@@ -15,13 +15,9 @@ require('dotenv').config()
 const database = knex({
     client: 'pg',
     // version: '15.1',
-    
     connection: {  
-        host : process.env.HOST,
-        user : "postgres",
-        password : "postgres",
-        database : process.env.DATABASE,
-        timezone: 'utc'
+        host : process.env.DATABASE_URL,
+        ssl: true
 }
   });  
 
@@ -134,7 +130,8 @@ app.post('/upload',upload,(req, res) => {
             database.transaction(trx =>{
         trx.insert({
             name:name,
-            url:data.Location,
+            url: data.Location,
+            key: data.key,
             email:email
         })
         .into('data') 
@@ -151,48 +148,53 @@ app.post('/upload',upload,(req, res) => {
 
 app.get('/download', function (req, res) {
     const { email } = req.body;
-    database.select('url').from('data').where('email', '=', email)
-         .then(user => {
-            database.select('foldername').from('data').where('email', '=', email)
-                .then((name) => {
-                    const url = (user[0].url)
-                    const directory = (path.join(__dirname, '../../'))
-                    const filename = path.basename(url)
-                    if (name[0].foldername != "") {
+    database.select('url',"safe").from('data').where('email', '=', email)
+        .then(user => {
+            if (user[0].safe != "unsafe") {
+                database.select('foldername').from('data').where('email', '=', email)
+                    .then((name) => {
+                        const url = (user[0].url)
+                        const directory = (path.join(__dirname, '../../'))
+                        const filename = path.basename(url)
+                        if (name[0].foldername != "") {
                         
-                        https.get(url, (res) => {
-                            // Image will be stored at this path
-                            const filePath = fs.createWriteStream(`${directory}/downloads/${name[0].foldername}/${filename}`);
-                            res.pipe(filePath);
-                            filePath.on('finish', () => {
-                                filePath.close();
-                                console.log('Download Completed1');
+                            https.get(url, (res) => {
+                                // Image will be stored at this path
+                                const filePath = fs.createWriteStream(`${directory}/downloads/${name[0].foldername}/${filename}`);
+                                res.pipe(filePath);
+                                filePath.on('finish', () => {
+                                    filePath.close();
+                                    console.log('Download Completed1');
                               
-                            })
+                                })
                             
-                        })
+                            })
                            
-                    }
-                else {
-                                 const location2 = `${directory}downloads/${filename}`;
-                                 https.get(url, (res) => {
-                              // Image will be stored at this path
+                        }
+                        else {
+                            const location2 = `${directory}downloads/${filename}`;
+                            https.get(url, (res) => {
+                                // Image will be stored at this path
              
-                              const filePath = fs.createWriteStream(location2);
-                              res.pipe(filePath);
-                              filePath.on('finish', () => {
-                                  filePath.close();
-                                  res.status(200).json("success")
-                                  console.log('Download Completed2');
-                              })
-                          })
-                             }
-                 res.status(200).json("success")
-              
+                                const filePath = fs.createWriteStream(location2);
+                                res.pipe(filePath);
+                                filePath.on('finish', () => {
+                                    filePath.close();
+                                    res.status(200).json("success")
+                                    console.log('Download Completed2');
+                                })
+                            })
+                        }
+                        res.status(200).json("success")
+               
                      
-        })
+                    })
+           
             .catch((error)=>{res.status(400).json(error) })
-          
+            }
+            else {
+        res.status(400).json("UNSAFE FILE")
+    }
       })
     .catch((error) => { res.status(400).json(error) })
     
@@ -226,7 +228,110 @@ app.post("/makefolder", (req, res) => {
 })
 
 
+app.post('/AdminRegister',(req,res) =>{
+    const{email, name ,password} = req.body
+    if (!email || !name || !password) {
+        return res.status(400).json("incorrect form submission")
+    }
+    const hash = bcrypt.hashSync(password)
+    database.transaction(trx =>{
+        trx.insert({
+            name:name,
+            password:hash,
+            email: email,
+            admin:"admin"
+        })
+        .into('aws') 
+            .then(() =>{ 
+                res.status(200).json("Admin created")
+                })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+        .catch(err => res.status(400).json(err))
+    
+})
+
+app.post ('/AdminSignin', (req,res) => {
+    const{email ,password} = req.body
+    if (!email  || !password) {
+        return res.status(400).json("incorrect form submission")
+    }
+   database.select('email' , "password" , "admin")
+  .from('aws')
+  .where('email' , "=" , email)
+  .then(data =>{
+      const isValid = bcrypt.compareSync(password, data[0].password)
+      const secondcheck = data[0].admin == "admin"
+    if (isValid && secondcheck) {
+       return database("aws").where({
+            email: email,
+            admin:  'admin'
+            }).select('*') 
+        
+        .then(user =>{
+            res.status(200).json(user[0])
+        })
+        .catch(err => res.status(400).json("cannot connect not an admin"))
+    }else{
+        res.status(400).json("Wrong credentials")
+    }
+  })
+ 
+      
+  
+  .catch(err => res.status(400).json("Wrong credentials"))
+})
+
+app.post("/unsafe", (req, res) => {
+    const { email, password, name } = req.body
+    if (!email || !password || !name) {
+        return res.status(400).json("incorrect form submission")
+    }
+    database.select('email', "password", "admin")
+        .from('aws')
+        .where('email', "=", email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(password, data[0].password)
+            const secondcheck = data[0].admin == "admin"
+            if (isValid && secondcheck) {
+                database.select("key").from("data").where("name", "=", name)
+               .then((data) => {
+                database('data')
+                    .where("name", "=", name)
+                    .update({
+                        safe: "unsafe"
+                    }).then(() => {
+                        s3.deleteObject({ Bucket: process.env.AWS_BUCKET_NAME, Key: data[0].key }, (err, data) => {
+                            console.error(err);
+                            console.log(data);
+                            res.status(200).json("done")
+                        });
+                    })
+                    .catch(() => res.status(400).json("error"))
+                 })
+                .catch(() => res.status(400).json("error"))
+            }
+        })
+        })
 
 
+app.get("/stream", (req, res) => {
+     const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        ContentType: ('video/mp4',"audio/mp4","audio/mpeg"),
+        ACL:'public-read',
+        Key: `${uuid()}.${fileType}`,
+        Body: req.file.buffer
+    }
+    s3.getObject(params)
+    .on('httpHeaders', function (statusCode, headers) {
+        res.set('Content-Length', headers['content-length']);
+        res.set('Content-Type', headers['content-type']);
+        this.response.httpResponse.createUnbufferedStream()
+            .pipe(res);
+    })
+    .send();
+})
 
-app.listen(4000 ,() =>{console.log("running on port 4000")})
+app.listen(4000 || process.env.PORT ,() =>{console.log("running on port 4000")})
