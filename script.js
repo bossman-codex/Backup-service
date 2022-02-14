f242bf19a9a97b09628d03a805b6c68f34399bc0
bossman-codex <56489079+bossman-codex@users.noreply.github.com>
2022-02-14 21:37:26 +0100

    added procfile

--- script.js
+++ script.js
@@ -21,11 +21,7 @@ const database = knex({
 }
   });  
 
-//   const pg = require('knex')({
-//   client: 'pg',
-//   connection: process.env.PG_CONNECTION_STRING,
-//   searchPath: ['knex', 'public'],
-// });
+
 
 
 const app = express()
